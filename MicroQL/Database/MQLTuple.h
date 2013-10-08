99075998d7a4ff14ff93eb2bcfa3902c681c0cea
Hendri <emailhendri@gmail.com>
2013-10-08 11:38:25 +0800

    Added tuple manager

---
+++ MicroQL/Database/MQLTuple.h
@@ -0,0 +1,19 @@
+#include "MQLColumn.h"
+#include <list>
+
+using namespace std;
+class MQLTupleManager;
+
+class  MQLTuple {
+public:
+	MQLTuple(MQLTupleManager *parent) {
+		myParent = parent;
+	}
+	void addValue(string value) {
+		valueList.push_back(value);
+	}
+private:
+	//vector<MQLColumn> column;
+	list<string> valueList;
+	MQLTupleManager *myParent;
+};
---
+++ MicroQL/Database/MQLTupleManager.h
@@ -0,0 +1,14 @@
+#include <vector>
+#include <list>
+#include "MQLTuple.h";
+#include "MQLColumn.h"
+
+typedef vector<MQLTuple> TupleList;
+
+class TupleManager {
+public :
+	void addColumnList(MQLColumn);
+private:
+	vector<MQLColumn> columnList;
+	TupleList tupleList;
+}
